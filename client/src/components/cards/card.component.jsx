76aea2d49763f6fb2fb43e4caa6deebe30c6867a
AngelSilva98 <angelceesd@gmail.com>
2023-07-06 20:34:33 -0300

    saqué el archivo index

--- client/src/components/cards/card.component.jsx
+++ client/src/components/cards/card.component.jsx
@@ -2,10 +2,12 @@ function Cards({ trainer }) {
   const { nombre, apellido, tipoUsuario, valoracion, imagen, id } = trainer;
   return (
     <div>
-      <img src={imagen} alt="imagen not found" />
-      <h2>{tipoUsuario}</h2>
-      <h2>{nombre + apellido}</h2>
-      <p>Valoración:{valoracion}</p>
+      <Link to={`/home/${id}`}>
+        <img src={imagen} alt="imagen not found" />
+        <h2>{nombre + apellido}</h2>
+        <h2>{tipoUsuario}</h2>
+        <p>Valoración:{valoracion}</p>
+      </Link>
     </div>
   );
 }
--- index.js
+++
@@ -1,3 +0,0 @@
-console.log("HOLA");
-
-console.log("log angel");
