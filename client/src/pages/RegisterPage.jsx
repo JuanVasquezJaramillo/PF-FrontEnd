074ee9b529d5ccc578d32d4f1e29c7fc1685d029
GavidiaS <gavdia1302@gmail.com>
2023-07-06 14:08:32 -0500

    first commit Santiago

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import Login from "../components/Login";
+
+export default function RegisterPage() {
+  return (
+    <>
+      <h1>Register Page</h1>
+      <Login option="register" />
+    </>
+  );
+}
