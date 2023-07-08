230ee6701a4ea91a1a41e52b28f3ea1d54bd86ab
JuanVasquezJaramillo <juapablovasquez2003@gmail.com>
2023-07-07 21:17:07 -0500

    Component UserProfile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Navbar from './components/Navbar';
+import UserProfile from './components/userProfile'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='profile' element={<UserProfile/>} />
       </Routes>
     </>
   )
