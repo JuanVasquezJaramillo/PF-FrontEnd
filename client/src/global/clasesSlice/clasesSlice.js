b4bb9520950a1e987f40cced372b7556b8e267f3
JuanVasquezJaramillo <124230164+JuanVasquezJaramillo@users.noreply.github.com>
2023-07-12 13:05:43 -0500

    Update clasesSlice.js

--- client/src/global/clasesSlice/clasesSlice.js
+++ client/src/global/clasesSlice/clasesSlice.js
@@ -48,33 +48,3 @@ export const getAllClass = createAsyncThunk("clases/getAllClass", async () => {
 
 export const {filterTypeExercise, orderByPrice } = clasesSlice.actions
 export default clasesSlice.reducer;
-import { createSlice } from "@reduxjs/toolkit";
-export const  clasesSlice = createSlice({
-    name: "clases",
-    initialState:{
-        list:[],  
-    },
-    reducers:{
-        setClaseslist:(state, action)=>{
-            state.list = action.payload     
-        }
-    }   
-})
-
-export default clasesSlice.reducer;
-export const {setClaseslist}= clasesSlice.actions
-
-
-import axios from "axios";
-export function getAllClases(){
-    return async function (dispatch){
-        try {
-            const response = (await axios.get("http://localhost:3001/videos/getvideo")).data
-          
-            dispatch(setClaseslist(response))
-
-        } catch (error) {
-            alert("Sucedio un error al requerir las clases")
-        }
-    }
-}
