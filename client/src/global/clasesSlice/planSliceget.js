ea9cb4d12d2484eb1adbd8dc6f1c7ced2728bcbc
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-07-12 19:03:35 -0500

    nueov

--- client/src/global/clasesSlice/planSliceget.js
+++ client/src/global/clasesSlice/planSliceget.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export function getPlan() {
     return async function () {  
         try {
-        const response = await axios.get("http://localhost:5000/user/");
+        const response = await axios.get("http://localhost:5000/plans/");
             alert("la informacion se guardo de forma exitosa")
         } catch (error) {
            
