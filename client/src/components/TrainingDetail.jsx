3e3a6f555c01e67bc2ab7c18580edc9ba2f91de5
Daniel <danitelo@gmail.com>
2023-07-19 06:11:17 -0300

    trainingDetail con axios

--- client/src/components/TrainingDetail.jsx
+++ client/src/components/TrainingDetail.jsx
@@ -3,40 +3,41 @@ import axios from 'axios';
 import { useEffect } from "react";
 import { useState } from "react";
 import Carrousel from "./carrousel";
-import style from "../modules/trainingDetail.module.css";
+import style from "../modules/TrainingDetail.module.css";
 
 const TrainingDetail = () => {
   const {idPlan} = useParams();
   console.log(idPlan)
-  const [training, setTraining] = useState({});
+  const [plan, serPlan] = useState({})
   useEffect(() => {
    const URL_BASE='http://127.0.0.1:5000/plan' 
    axios(`${URL_BASE}/${idPlan}`)
-    .then(response=>setTraining(response.data))}
+    .then(response=>serPlan(response.data))}
    
   , []);
   const handleBorrar = async ()=>{
     const URL_BASE='http://127.0.0.1:5000/plan'
-   axios.delete(`${URL_BASE}?idPlan=${idPlan}`)
-   .then(response=>setTraining(response.data))
-   .catch(error => {
-     console.error(error.message);
+    axios.delete(`${URL_BASE}?idPlan=${idPlan}`)
+    .then(response=>serPlan(response.data))
+    .catch(error => {
+      console.error(error.message);
    })
  };
   return (
     <div className={style.container}>
-      {training.title ? ( 
+      {plan.title ? ( 
         <div className={style.descripcionTraining}> 
 
-          {training.videos ? <Carrousel videos={training.videos} /> : null}
-          <div className={style.datos}>          <button onClick={() => handleBorrar()} className={style.button}>
-            borrar plan
-          </button>
-            <p className={style.parrafos}>Entrenador: {training.idUser}</p>
-            <p className={style.parrafos}>{training.publicDescription}</p>
-            <p className={style.parrafos}>{training.privateDescription}</p>
-            <p className={style.parrafos}>Precio: {training.price}</p>
-            <p className={style.parrafos}>Etiquetas: {training.tags}</p>
+          {plan.videos ? <Carrousel videos={plan.videos} /> : null}
+          <div className={style.datos}>
+            <button onClick={() => handleBorrar()} className={style.button}>
+              borrar plan
+            </button>
+            <p className={style.parrafos}>Entrenador: {plan.idUser}</p>
+            <p className={style.parrafos}>{plan.publicDescription}</p>
+            <p className={style.parrafos}>{plan.privateDescription}</p>
+            <p className={style.parrafos}>Precio: {plan.price}</p>
+            <p className={style.parrafos}>Etiquetas: {plan.tags}</p>
           </div>
           <textarea
             name="comentario"
