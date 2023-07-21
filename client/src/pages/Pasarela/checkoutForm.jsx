b05ba25da41cf93c0936f6903aecdeb5b00eef91
JuanVasquezJaramillo <juapablovasquez2003@gmail.com>
2023-07-21 15:59:00 -0500

    Se agregó el mailing 21/07/2023. 16:58.

--- client/src/pages/Pasarela/checkoutForm.jsx
+++ client/src/pages/Pasarela/checkoutForm.jsx
@@ -3,14 +3,23 @@ import "bootswatch/dist/Lux/bootstrap.min.css" //Importación de tema predetermi
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { postCheckoutId } from "../../global/pagosSlice/pagosSlice";
-import { useAuth } from "../../context/AuthContext"; //Para que se agregue el nombre del comprador
+import { useAuth } from "../../context/authContext"; //Para que se agregue el nombre del comprador
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Swal from "sweetalert2";
+
 
 const CheckoutForm = ({ productos }) => {
+
     const auth = useAuth(); //borrar 
     const { displayName } = auth.user; //borrar
 
 
-    const [compraExitosa, setCompraExitosa] = useState(false);
+    //--mail--
+    const form = useRef();
+    //--fin--
+
+    const [loading, setloading] = useState(false);
 
 
     //CONFIG Stripe
@@ -19,6 +28,9 @@ const CheckoutForm = ({ productos }) => {
     //FIN
     const [total, setTotal] = useState(0)
     const dispatch = useDispatch();
+
+
+    //Método handle
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -31,30 +43,56 @@ const CheckoutForm = ({ productos }) => {
         })
         //-------FIN-------- 
         console.log("BANDER CHECKOUT", paymentMethod);
+
         //LOGICA PAGO    
 
-        if (displayName) {
-            paymentMethod.customer = displayName;
-        }else{
-            paymentMethod.customer = "NotFound"
+        if (!error) {
+
+            if (displayName) {
+                paymentMethod.customer = displayName;
+            } else {
+                paymentMethod.customer = "NotFound"
+            }
+
+
+            const { id } = paymentMethod
+            //paymentMethod.customer = "ADOLFO HOLA"
+            let mont = total / 4000
+            let objPay = {
+                id,
+                amount: mont * 100,
+            }
+
+            dispatch(postCheckoutId(objPay))
+
+            //FIN
+
+
+            //Lógica de mail 
+            emailjs.sendForm('service_6sqygb3', 'template_56xanjh', form.current, '1PabAE487WZsuh6hh')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+            //fin
+        } else {
+            Swal.fire({
+                title: 'Error',
+                text: "Ups! algo salió mal",
+                icon: "error"
+            })
+            emailjs.sendForm('service_6sqygb3', 'template_56xanjh', {to_name: displayName?displayName:"", user_email: auth?.user.email ? auth?.user.email : "", message: "Algo salió mal con tu compra :c"}, '1PabAE487WZsuh6hh')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
         }
-
-
-        const { id } = paymentMethod
-        //paymentMethod.customer = "ADOLFO HOLA"
-        let mont = total / 4000
-        let objPay = {
-            id,
-            amount: mont * 100,
-        }
-
-        dispatch(postCheckoutId(objPay))
-
-        //FIN
 
 
     }
-
+    //Fin método handle
 
     useEffect(() => {
         // Calcula el total cuando cambia el array de productos
@@ -65,15 +103,25 @@ const CheckoutForm = ({ productos }) => {
 
     return (
         <div style={{ textAlign: 'center', display: "flex", justifyContent: "center" }}>
-            <form className="card card-body" onSubmit={event => handleSubmit(event)}>
+
+            <form ref={form} className="card card-body" onSubmit={event => handleSubmit(event)}>
+
+
+
+                <label htmlFor="">Completa los datos para la factura</label>
+                <label>Nombre del comprador</label>
+                <input type="text" name="to_name" placeholder={displayName ? `${displayName}...` : ""} />
+                <label>Email al que enviar factura</label>
+                <input type="email" name="user_email" placeholder={auth?.user.email ? `${auth.user.email}...` : ""} />
 
                 <h3 className="text-center">TOTAL: $ {total}</h3>
-
                 <div className="form-group">
                     <CardElement className="form-control" />
                 </div>
+                <button className="btn btn-success" >
+                    COMPRAR
+                </button>
 
-                <button className="btn btn-success">COMPRAR</button>
             </form>
         </div>
     )
