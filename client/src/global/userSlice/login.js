3b9a7c438a3895762dc8fd2bc15eae845d4c3db5
AngelSilva98 <angelceesd@gmail.com>
2023-07-26 00:13:06 -0300

    about us cambios de lorem's

--- client/src/global/userSlice/login.js
+++ client/src/global/userSlice/login.js
@@ -1,19 +1,15 @@
 import axios from "axios";
-import {createAsyncThunk} from "@reduxjs/toolkit"
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export  const login = createAsyncThunk("user/login", 
-async (info) => {
-    console.log("",info)
- try {
-    const response = await axios.post("/login",info)
-   
-    const data = response.data
-    
-    return data
-    
- } catch (error) {
-    
-    alert(error.response.data)
- }
-})
+export const login = createAsyncThunk("user/login", async (info) => {
+  console.log("1111111111111111111111111111111111111111", info);
+  try {
+    const response = await axios.post("/login", info);
 
+    const data = response.data;
+    console.log("44444444444444444444444444444444444444", data);
+    return data;
+  } catch (error) {
+    alert(error.response.data);
+  }
+});
